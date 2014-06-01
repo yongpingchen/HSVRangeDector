b0d4f586d2844246980cd76c4d80e53017a85922
Yongping Chen <will.yp.chen@gmail.com>
2014-06-02 00:09:14 +0900

    add functions to convert uiimage to iplimage and convert from IPlimage

--- HSVMaskChecker/viewControllers/CYPViewController.mm
+++ HSVMaskChecker/viewControllers/CYPViewController.mm
@@ -9,12 +9,24 @@
 #import "CYPViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
 
+#import <opencv2/imgproc/imgproc_c.h>
+#import <CoreMedia/CoreMedia.h>
+#import <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace std;
+using namespace cv;
+
+
 @interface CYPViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 {
     NSArray *sliderArray;
     NSArray *valueLabelArray;
     
     UIPopoverController *objPopView;
+    
+    UIImage *originImage;
     
 }
 @property (weak, nonatomic) IBOutlet UISlider *minHValueSlider;
@@ -60,6 +72,39 @@
     UILabel *valueLabel = [valueLabelArray objectAtIndex:index];
     valueLabel.text = [NSString stringWithFormat:@"%d",value];
     
+    //mask the image
+    IplImage *iplImage = [CYPViewController CreateIplImageFromUIImage:originImage];
+    
+    IplImage *rgbImage = cvCreateImage(cvGetSize(iplImage), IPL_DEPTH_8U, 3);
+    cvCvtColor(iplImage, rgbImage, CV_BGR2RGB);
+    cvReleaseImage(&iplImage);
+
+    IplImage *imgRGB = cvCreateImage(cvGetSize(iplImage), IPL_DEPTH_8U, 3);
+    cvCvtColor(iplImage, imgRGB, CV_BGR2RGB);
+    Mat matRGB = Mat(imgRGB);
+    
+    //ipl imaeg is also converted to HSV; hue is used to find certain color
+    IplImage *imgHSV = cvCreateImage(cvGetSize(iplImage), 8, 3);
+    cvCvtColor(iplImage, imgHSV, CV_BGR2HSV);
+    
+    IplImage *imgThreshed = cvCreateImage(cvGetSize(iplImage), 8, 1);
+    
+    //it is important to release all images EXCEPT the one that is going to be passed to
+    //the didFinishProcessingImage: method and displayed in the UIImageView
+    cvReleaseImage(&iplImage);
+    
+    //filter all pixels in defined range, everything in range will be white, everything else
+    //is going to be black
+    //    cvInRangeS(imgHSV, cvScalar(_min, 100, 100), cvScalar(_max, 255, 255), imgThreshed);
+    cvInRangeS(imgHSV, cvScalar(67, 125, 255), cvScalar(67, 255, 255), imgThreshed);
+    
+    cvReleaseImage(&imgHSV);
+    
+    UIImage *convertedImage = [CYPViewController UIImageFromIplImage:imgThreshed];
+    [_photoImage setImage:convertedImage];
+    
+    cvReleaseImage(&imgThreshed);
+
 }
 
 - (IBAction)tapLoadPhoto:(id)sender {
@@ -107,6 +152,8 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
         [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
         UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
+        //keep a copy of origin image
+        originImage = [newImage copy];
         _photoImage.image = newImage;
         NSError *error;
         NSLog(@"error:%@",error.description);
@@ -119,6 +166,86 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
 	}
 }
 
++ (IplImage *)CreateIplImageFromUIImage:(UIImage *)image {
+    // Getting CGImage from UIImage
+    CGImageRef imageRef = image.CGImage;
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    // Creating temporal IplImage for drawing
+    IplImage *iplimage = cvCreateImage(
+                                       cvSize(image.size.width,image.size.height), IPL_DEPTH_8U, 4
+                                       );
+    // Creating CGContext for temporal IplImage
+    CGContextRef contextRef = CGBitmapContextCreate(
+                                                    iplimage->imageData, iplimage->width, iplimage->height,
+                                                    iplimage->depth, iplimage->widthStep,
+                                                    colorSpace, kCGImageAlphaPremultipliedLast|kCGBitmapByteOrderDefault
+                                                    );
+    // Drawing CGImage to CGContext
+    CGContextDrawImage(
+                       contextRef,
+                       CGRectMake(0, 0, image.size.width, image.size.height),
+                       imageRef
+                       );
+    CGContextRelease(contextRef);
+    CGColorSpaceRelease(colorSpace);
+    
+    
+    NSLog(@"image->width = %d" , iplimage->width);
+    NSLog(@"image->height = %d" , iplimage->height);
+    
+    // Creating result IplImage
+    IplImage *ret = cvCreateImage(cvGetSize(iplimage), IPL_DEPTH_8U, 3);
+    cvCvtColor(iplimage, ret, CV_RGBA2BGR);
+    cvReleaseImage(&iplimage);
+    
+    
+    
+    return ret;
+}
+
+
++(UIImage *)UIImageFromIplImage:(cv::Mat)aMat
+{
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    unsigned char* data = new unsigned char[4*aMat.cols * aMat.rows];
+    for (int y = 0; y < aMat.rows; ++y)
+    {
+        cv::Vec3b *ptr = aMat.ptr<cv::Vec3b>(y);
+        unsigned char *pdata = data + 4*y*aMat.cols;
+        
+        for (int x = 0; x < aMat.cols; ++x, ++ptr)
+        {
+            *pdata++ = (*ptr)[2];
+            *pdata++ = (*ptr)[1];
+            *pdata++ = (*ptr)[0];
+            *pdata++ = 0;
+        }
+    }
+    
+    // Bitmap context
+    CGContextRef context = CGBitmapContextCreate(data, aMat.cols, aMat.rows, 8, 4*aMat.cols, colorSpace, kCGImageAlphaNoneSkipLast);
+    
+    
+    
+    CGImageRef cgimage = CGBitmapContextCreateImage(context);
+    
+    UIImage *ret = [UIImage imageWithCGImage:cgimage scale:1.0
+                                 orientation:UIImageOrientationUp];
+    
+    CGImageRelease(cgimage);
+    
+    CGContextRelease(context);  
+    
+    // CGDataProviderRelease(provider);
+    
+    CGColorSpaceRelease(colorSpace);
+    
+    
+    return ret;
+}
 
 
 @end
